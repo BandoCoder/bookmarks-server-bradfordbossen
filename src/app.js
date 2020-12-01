81fc45ac78641588e23e5f92f0e8926014c249f2
kittybagpipes <sparticus978@gmail.com>
2020-12-01 13:43:54 -0800

    added winston

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
+const logger = require("./logger");
+const bookmarkRouter = require("./bookmarks/bookmarks-router");
 
 const app = express();
 
@@ -12,6 +14,7 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use(bookmarkRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, boilerplate; BRUV");
