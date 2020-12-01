dd656249f2c1f00e823c0324905512a6b9e990e7
kittybagpipes <sparticus978@gmail.com>
2020-12-01 14:46:23 -0800

    delete and get by id added

--- src/bookmarks/bookmark-routes.js
+++ src/bookmarks/bookmark-routes.js
@@ -6,6 +6,7 @@ const { bookmarks } = require("../store");
 const bookmarkRouter = express.Router();
 const bodyParser = express.json();
 
+// basic routes get and post
 bookmarkRouter
   .route("/bookmarks")
   .get((req, res) => {
@@ -38,6 +39,35 @@ bookmarkRouter
 
     logger.info(`Bookmarks with id ${id} created`);
     res.status(201).location(`http://localhost:8000/card/${id}`).json(bookmark);
+  });
+
+// id routes get post delete
+bookmarkRouter
+  .route("/bookmarks/:id")
+  .get((req, res) => {
+    const { id } = req.params;
+    const bookmark = bookmarks.find((b) => b.id == id);
+
+    //is there a bookmark with that id?
+    if (!bookmark) {
+      logger.errror(`Bookmark with id:${id} not found.`);
+      return res.status(404).send("Error 404 Not Found");
+    }
+    res.json(bookmark);
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+
+    const bookmarkIndex = bookmarks.findIndex((b) => b.id == id);
+
+    if (bookmarkIndex === -1) {
+      logger.error(`Bookmark with id:${id} not found`);
+      return res.status(404).send("Error 404 Not Found");
+    }
+    bookmarks.splice(bookmarkIndex, 1);
+
+    logger.info(`Bookmark with id:${id} deleted`);
+    res.status(204).end();
   });
 
 module.exports = bookmarkRouter;
