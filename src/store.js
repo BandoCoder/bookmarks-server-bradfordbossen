27e56b7a72ff893e95813106bc1de41a443b6b4d
kittybagpipes <sparticus978@gmail.com>
2020-12-03 05:21:41 -0800

    ready to deploy again

--- src/store.js
+++ src/store.js
@@ -6,13 +6,6 @@ const bookmarks = [
     description: "curriculum",
     rating: 5,
   },
-  {
-    id: "cki6ix7hq000604w3ipp4sgyq",
-    title: "Your mom",
-    url: "http://www.yourmomshouse.com",
-    description: "description",
-    rating: 1,
-  },
 ];
 
 module.exports = { bookmarks };
